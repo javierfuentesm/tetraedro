65252ebe08c7bf0cd101bec5ce987d4b45bcc4fc
rjfm <javier.fuentesm@hotmail.com>
2019-11-05 20:36:08 -0600

    Terminado

---
+++ app/src/main/java/com/example/cubo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.cubo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Lienzo l = new Lienzo(this);
+        setContentView(l);
+    }
+}
